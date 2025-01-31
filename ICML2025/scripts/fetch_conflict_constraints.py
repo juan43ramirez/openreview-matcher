5c3de0162b6217c3f0a3b5eb547f388a8e91e75c
juan43ramirez <juan43.ramirez@gmail.com>
2025-01-31 11:18:20 -0500

    `fetch_conflict_constraints.py`

---
+++ ICML2025/scripts/fetch_conflict_constraints.py
@@ -0,0 +1,58 @@
+import argparse
+import pandas as pd
+import openreview
+import os
+
+# Use environment variables to store the username and password
+OR_USERNAME = os.environ.get('OPENREVIEW_USERNAME')
+OR_PASSWORD = os.environ.get('OPENREVIEW_PASSWORD')
+
+CONFERENCE_ID = 'ICML.cc/2025/Conference'
+
+CLIENT_V1 = openreview.Client(
+    baseurl='https://api.openreview.net',
+    username=OR_USERNAME,
+    password=OR_PASSWORD,
+)
+
+CLIENT_V2 = openreview.api.OpenReviewClient(
+    baseurl='https://api2.openreview.net',
+    username=OR_USERNAME,
+    password=OR_PASSWORD
+)
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--output", type=str, help="Output file")
+
+    args = argparser.parse_args()
+
+    print(f"Fetching reviewer conflicts")
+
+    # ---------------------------------------------------------
+    # Get conflicts
+    # ---------------------------------------------------------
+
+    rev_conflicts=CLIENT_V2.get_grouped_edges(
+        invitation='ICML.cc/2025/Conference/Reviewers/-/Conflict',
+        groupby='tail',
+        select='head'
+    )
+
+    # ---------------------------------------------------------
+    # Translate conflicts to constraints
+    # ---------------------------------------------------------
+    print(f"Translating conflicts to constraints")
+
+    constraints = []
+    for conflict in rev_conflicts:
+        reviewer = conflict['id']['tail']
+        for c in conflict['values']:
+            paper = c['head']
+            constraints.append((paper, reviewer, -1))
+
+    df = pd.DataFrame(constraints)
+    df.to_csv(args.output, index=False, header=False)
+
+    print(f"Done. Extracted {len(df)} conflict constraints")
+
