ba969731edc7c5d398c38a20d83c0c15240f0c6d
Sébastien Lachapelle <sebastien.lachapelle@live.com>
2025-01-31 09:27:42 -0500

    add env install

--- ICML2025/scripts/secure-paper-bidding.sh
+++ ICML2025/scripts/secure-paper-bidding.sh
@@ -1,3 +1,7 @@
+#conda create -n secure-paper-bidding python=3.10
+#conda activate secure-paper-bidding
+#pip install ortools scipy torch numpy tqdm openreview-py
+
 cd ICML2025/secure-paper-bidding
 bash scripts/init_and_data_process_script.sh
 bash scripts/assignment_script.sh # train model
