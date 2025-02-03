dd67d289a2151aa8c7af511aa07b8483d759bae7
Sébastien Lachapelle <sebastien.lachapelle@live.com>
2025-02-03 18:25:53 -0500

    more features

--- ICML2025/scripts/telltail.py
+++ ICML2025/scripts/telltail.py
@@ -44,10 +44,14 @@ def generate_telltail_start_list(G, start_v, rng_init):
     S_starts = [neighbors > 0] + [rng_init.random(G.shape[0]) > rng_init.random(1) for _ in range(10)]
     return S_starts
 
-def run_telltail(G, start_v, rng_init):
+def run_telltail(G, start_v, rng_init, graph_type='intersection'):
     if np.any(G != G.T):
-        G_undir = np.clip(G * G.T, 0, 1) # intersection
-        graph_type = 'intersection'
+        if graph_type == 'intersection':
+            G_undir = np.clip(G * G.T, 0, 1)
+        elif graph_type == "union":
+            G_undir = np.clip(G + G.T, 0, 1)
+        else:
+            raise NotImplementedError("graph_type must be 'intersection' or 'union' when G != G.T")
     else:
         G_undir = G
         graph_type = 'none'
@@ -181,25 +185,32 @@ def get_authors():
     all_submissions = submissions_v1 + submissions_v2
     assert len(submissions_v1) == 0
     print("no submissions from the v1 API")  # not sure why that is.
+    #print("# submissions from the v2 API:", len(submissions_v2))
 
     author2sub_ids = {}
+    all_submission_ids = []
     for submission in all_submissions:
+        all_submission_ids.append(submission.id)
         for author_id in submission.content['authorids']['value']:
             if author_id not in author2sub_ids.keys():
                 author2sub_ids[author_id] = []
             author2sub_ids[author_id].append(submission.id)
 
-    return author2sub_ids
+    return author2sub_ids, np.unique(all_submission_ids)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run TellTail algorithm')
     parser.add_argument('--bids', type=str, default='ICML2025/data/numeric_bids.csv')
+    #parser.add_argument('--filtered_file', type=str, default='ICML2025/data/filtered_numeric_bids.csv')
+    parser.add_argument('--undirected_type', type=str, default='union', choices=['union', 'intersection'])
 
     args = parser.parse_args()
+    folder = args.bids.replace(os.path.basename(args.bids), '')
 
     bids = pd.read_csv(args.bids, header=None, index_col=None)
 
+    total_num_bids = len(bids)
     all_submission_ids = bids[0].unique()
     all_reviewer_ids = bids[1].unique()
 
@@ -209,43 +220,119 @@ if __name__ == "__main__":
         if bid > 0:
             rev2bids[rev_id].append(sub_id)
 
-    authors2sub_ids = get_authors()
+    authors2sub_ids, all_submission_ids_ = get_authors()
+    print("Number of reviewers (bid file):", len(set(all_reviewer_ids)))
+    print("Number of authors (API):", len(authors2sub_ids))
+    print("Intersection of the above two:", len(set(authors2sub_ids.keys()) & set(all_reviewer_ids)))
+    print("Number of Submissions (API):", len(all_submission_ids_))
+    print("Number of Submissions (bid file):", len(all_submission_ids))
+    print("Intersection of the above two:", len(set(all_submission_ids) & set(all_submission_ids_)))
+
     # add reviewers that are not authors to this list
     for rev_id in all_reviewer_ids:
         if rev_id not in authors2sub_ids.keys():
             authors2sub_ids[rev_id] = []
+        #else:
+        #    print(f"reviewer {rev_id} authored a submission.")
 
     # Build the reviewer-reviewer matrix.
-    rev_rev_matrix = np.zeros((len(all_reviewer_ids), len(all_reviewer_ids)))
     rev_id2index = {}
     rev_index2id = {}
     for index, rev_id in enumerate(all_reviewer_ids):
         rev_id2index[rev_id] = index
         rev_index2id[index] = rev_id
 
+    revrev2sub_ids = {}
+    rev_rev_matrix = np.zeros((len(all_reviewer_ids), len(all_reviewer_ids)))
     for rev1 in all_reviewer_ids:
         for rev2 in all_reviewer_ids:
-            edge = len(list(set(rev2bids[rev1]) & set(authors2sub_ids[rev2]))) > 0
-            rev_rev_matrix[rev_id2index[rev1], rev_id2index[rev2]] = edge
+            revrev2sub_ids[(rev1, rev2)] = set(rev2bids[rev1]) & set(authors2sub_ids[rev2])  # papers of rev2 on which rev1 bidded
+            rev_rev_matrix[rev_id2index[rev1], rev_id2index[rev2]] = len(revrev2sub_ids[(rev1, rev2)]) > 0
+            if rev1 == rev2: assert len(revrev2sub_ids[(rev1, rev2)]) == 0
 
     # DEBUGGING
-    print("WARNING: Using a random graph for debugging.")
-    rev_rev_matrix = np.random.binomial(n=1, p=0.5, size=(10,10))
+    #print("WARNING: Using a random graph for debugging.")
+    #rev_rev_matrix = np.random.binomial(n=1, p=0.5, size=(10,10))
 
-    print("Number of edges in the directed graph:", rev_rev_matrix.sum())
-    print("Number of edges in the undirected graph:", (rev_rev_matrix * rev_rev_matrix.T).sum())
+    num_trials = 10
+    all_suspicious_bids = []
+    all_suspicious_bids_set = set([])
+    all_suspicious_rev = []
+    for trial in range(num_trials):
+        print(f"Trial #{trial}")
+        print("Number of edges in the directed graph:", rev_rev_matrix.sum())
+        print("Number of edges in the undirected graph (intersect):", (rev_rev_matrix * rev_rev_matrix.T).sum())
+        print("Number of edges in the undirected graph (union):", ((rev_rev_matrix + rev_rev_matrix.T) > 0).sum())
+        #print("Number of reviewers:", rev_rev_matrix.shape[0])
 
-    # Run TellTail algorithm
-    rng_init = np.random.RandomState(SEED)
-    start_v = get_local_search_init(rev_rev_matrix)
-    #start_v = np.argmax(rev_rev_matrix.sum(axis=0))  # Hack
-    S_detect, method_param_string = run_telltail(rev_rev_matrix, start_v, rng_init)
+        # Run TellTail algorithm
+        rng_init = np.random.RandomState(SEED)
+        start_v = get_local_search_init(rev_rev_matrix)
+        #start_v = np.argmax(rev_rev_matrix.sum(axis=0))  # Hack
+        print("Running TellTail...")
+        S_detect, method_param_string = run_telltail(rev_rev_matrix, start_v, rng_init, graph_type=args.undirected_type)
+        suspicious_rev_ids = [rev_index2id[index] for index in S_detect]
+        all_suspicious_rev.append(suspicious_rev_ids)
+        print("Potential colluders:", suspicious_rev_ids)
 
-    S_detect_ids = [rev_index2id[index] for index in S_detect]
-    print("Potential colluders:", S_detect_ids)
+        # remove bids from the rev_rev_matrix for the next trial
+        for suspicious_index in S_detect:
+            rev_rev_matrix[suspicious_index, S_detect] = 0
+
+        # record suspicious bids
+        suspicious_bids = []
+        for rev1 in suspicious_rev_ids:
+            for rev2 in suspicious_rev_ids:
+                for sub_id in revrev2sub_ids[(rev1,rev2)]:
+                    suspicious_bids.append((sub_id, rev1))
+        all_suspicious_bids.append(set(suspicious_bids))
+        all_suspicious_bids_set = all_suspicious_bids_set | set(suspicious_bids)
+
+        print(f"Number of bids removed: {len(all_suspicious_bids_set)}/{total_num_bids}")
+        if len(all_suspicious_bids_set) >= 0.01 * total_num_bids:
+            break
+
+    # producing list of removed bids
+    filename = os.path.join(folder, "removed_bids.csv")
+    if os.path.exists(filename):
+        os.remove(filename)
+    with open(filename, "w", newline='') as file:
+        for trial in range(len(all_suspicious_bids)):
+            file.write(f"Trial #{trial}")
+            file.write('\n')
+            for suspicious_bid in all_suspicious_bids[trial]:
+                sub_id, rev_id = suspicious_bid
+                file.write(f"{sub_id},{rev_id}")
+                file.write('\n')
+
+    # producing filtered csv file
+    filename = os.path.join(folder, "filtered_bids.csv")
+    if os.path.exists(filename):
+        os.remove(filename)
+    with open(filename, "w", newline='') as file:
+        for i, row in bids.iterrows():
+            sub_id, rev_id, bid = row
+            if (sub_id,rev_id) in all_suspicious_bids:
+                print("Dropping line:", sub_id, rev_id, bid)
+            else:
+                file.write(f"{sub_id},{rev_id},{bid}")
+                file.write('\n')
+
 
 """
-I'm facing this error when using the ICML2024 subset of bids... but code runs with a random graph
+HOW TO USE:
+- Might want to change the CONFERENCE_ID on line 12 to "2025".
+- Give path to cvs file containing bids via the --bids argument.
+- Code will produce two files in the folder containing the bid csv file: 
+    - removed_bids.csv : List of bids that were removed at each trial.
+    - filtered_bids.csv : List of original bids but without suspicious bids.
+"""
+
+
+
+"""
+I'm facing this error when using the ICML2024 subset of bids... 
+Code runs with a random graph (changing to 'union' instead of 'intersection' also worked) 
 
 /home/mila/l/lachaseb/openreview-matcher/ICML2025/scripts/telltail.py:68: RuntimeWarning: invalid value encountered in divide
   M = A - np.outer(d, d) / (2 * m)
